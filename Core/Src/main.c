4b2c51af84bd9e27aa18053306328ac72bf7cd3e
Van Son <thanh.duong.vs@gmail.com>
2023-06-04 08:06:02 +0700

    update lvgl

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -7,14 +7,28 @@
 #include "image.h"
 #include "LCD_Test.h"
 
+#include "lvgl.h"
+#include "lv_port_disp.h"
+
 int main(void)
 {
     board_init();
     printf("\nStart Program!\n");
 
     LCD_2in_test();
+
+    lv_init();
+    lv_port_disp_init();
+
+    lv_obj_t* btn = lv_btn_create(lv_scr_act()); 
+    lv_obj_set_pos(btn, 10, 10);
+    lv_obj_set_size(btn, 120, 50);
+    lv_obj_t* label = lv_label_create(btn);
+    lv_label_set_text(label, "OneKiwi");
+    lv_obj_center(label);
     while (1)
     {
+        lv_task_handler();
     }
 }
 
